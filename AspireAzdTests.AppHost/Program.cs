632563ecf26499a2baad928757e3e9f3e868eef3
bradygaster <bradyg@microsoft.com>
2024-01-17 18:51:26 -0800

    formatting

--- AspireAzdTests.AppHost/Program.cs
+++ AspireAzdTests.AppHost/Program.cs
@@ -1,32 +1,32 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache           = // redis instance the app will use for output caching
-        builder.AddRedis("cache");
+// redis instance the app will use for output caching
+var redisCache      = builder.AddRedis("cache");
 
-var pubsub          = // redis instance the app will use for simple messages
-        builder.AddRedis("pubsub");
+// redis instance the app will use for simple messages
+var redisPubSub     = builder.AddRedis("pubsub");
 
-var azureStorage    = // azure storage account the app will use for blob & table storage
-        builder.AddAzureStorage("storage")
-               .UseEmulator();
+// azure storage account the app will use for blob & table storage
+var azureStorage    = builder
+                        .AddAzureStorage("storage")
+                            .UseEmulator();
 
-var peopleTable     = // azure table storage for storing people data
-        azureStorage
-               .AddTables("requestlog");
+// azure table storage for storing people data
+var peopleTable     = azureStorage.AddTables("requestlog");
 
-var markdownBlobs   = // azure blob storage for storing markdown files
-        azureStorage
-               .AddBlobs("markdown");
+// azure blob storage for storing markdown files
+var markdownBlobs   = azureStorage.AddBlobs("markdown");
 
-var apiservice      = // the back-end API the front end will call
-        builder.AddProject<Projects.AspireAzdTests_ApiService>("apiservice");
+// the back-end API the front end will call
+var apiservice      = builder.AddProject<Projects.AspireAzdTests_ApiService>("apiservice");
 
-_                   = // the front end app
-        builder.AddProject<Projects.AspireAzdTests_Web>("webfrontend")
-               .WithReference(cache)
-               .WithReference(pubsub)
-               .WithReference(apiservice)
-               .WithReference(peopleTable)
-               .WithReference(markdownBlobs);
+// the front end app
+_                   = builder
+                        .AddProject<Projects.AspireAzdTests_Web>("webfrontend")
+                           .WithReference(redisCache)
+                           .WithReference(redisPubSub)
+                           .WithReference(apiservice)
+                           .WithReference(peopleTable)
+                           .WithReference(markdownBlobs);
 
 builder.Build().Run();
